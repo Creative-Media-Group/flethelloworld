d11a1440f2344dec1c62473583fc667e5dcc0bf5
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-06 00:46:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.add(ft.SafeArea(ft.Text("Hello, Flet!", style="center")))
 
 
 ft.app(main)
