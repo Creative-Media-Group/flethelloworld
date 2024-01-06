5525e73b57dfcd29e9b75706e2860a7d88eb081d
tct123 <42028373+tct123@users.noreply.github.com>
2024-01-06 02:07:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,24 @@ import flet as ft
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!", style="center")))
 
+    def on_dialog_result(e: ft.FilePickerResultEvent):
+        print("Selected files:", e.files)
+        print("Selected file or directory:", e.path)
+
+    file_picker = ft.FilePicker(on_result=on_dialog_result)
+
+    def upload_files(e):
+        upload_list = []
+        if file_picker.result != None and file_picker.result.files != None:
+            for f in file_picker.result.files:
+                upload_list.append(
+                    ft.FilePickerUploadFile(
+                        f.name,
+                        upload_url=page.get_upload_url(f.name, 600),
+                    )
+                )
+            file_picker.upload(upload_list)
+        page.add(ft.ElevatedButton("Click me!", on_click=upload_files))
+
 
 ft.app(main)
